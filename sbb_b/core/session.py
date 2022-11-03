61fcddc60574e7c12d1ace52a175605406b707a5
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-03 09:42:17 +0300

    Update session.py

--- sbb_b/core/session.py
+++ sbb_b/core/session.py
@@ -14,15 +14,20 @@ __version__ = "2.10.6"
 loop = None
 
 if Config.STRING_SESSION:
-    session = StringSession(str(Config.STRING_SESSION))
+    session = bothseesion(Config.STRING_SESSION, LOGS)
 else:
     session = "jmthon"
 
 try:
     sbb_b = JmthonClient(
-        bothseesion(Config.STRING_SESSION, LOGS),
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
         app_version=__version__,
-        device_model="JMTHON",
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
     )
 
 sbb_b.tgbot = tgbot = JmthonClient(
