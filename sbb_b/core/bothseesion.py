8c416181bf3adc9eacc11f5e32de6c92e5bb780b
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-03 07:21:39 +0300

    Create bothseesion.py

---
+++ sbb_b/core/bothseesion.py
@@ -0,0 +1,70 @@
+
+import base64
+import os
+
+try:
+    import ipaddress
+except ModuleNotFoundError:
+    os.system("pip3 install ipaddress")
+    import ipaddress
+    
+try:
+    import struct
+except ModuleNotFoundError:
+    os.system("pip3 install struct")
+    import struct
+    
+import sys
+
+from telethon.errors.rpcerrorlist import AuthKeyDuplicatedError
+from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringSession
+
+from ..Config import Config
+
+# https://github.com/pyrogram/pyrogram/blob/master/docs/source/faq/what-are-the-ip-addresses-of-telegram-data-centers.rst
+PYROFORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
+
+
+DC_IPV4 = {
+    1: "149.154.175.53",
+    2: "149.154.167.51",
+    3: "149.154.175.100",
+    4: "149.154.167.91",
+    5: "91.108.56.130",
+}
+
+
+def bothseesion(session, logger=LOGS):
+    if session:
+        # كود التيليثون
+        if session.startswith(CURRENT_VERSION):
+            if len(session.strip()) != 353:
+                logger.exception("كود سيشن تيليثون غير صحيح يرجى وضعه بشكل صحيح")
+                sys.exit()
+            return StringSession(session)
+        # كود البايروجرام
+        elif len(session) in PYROFORM.keys():
+            if len(session) in [351, 356]:
+                dc_id, _, auth_key, _, _ = struct.unpack(
+                    PYROFORM[len(session)],
+                    base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
+                )
+            else:
+                dc_id, _, _, auth_key, _, _ = struct.unpack(
+                    PYROFORM[len(session)],
+                    base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
+                )
+            return StringSession(
+                CURRENT_VERSION
+                + base64.urlsafe_b64encode(
+                    struct.pack(
+                        _STRUCT_PREFORMAT.format(4),
+                        dc_id,
+                        ipaddress.ip_address(DC_IPV4[dc_id]).packed,
+                        443,
+                        auth_key,
+                    )
+                ).decode("ascii")
+            )
+    logger.exception("لم يتم ايجاد كود سيشن لذلك توقفت العملية"))
+    sys.exit()
