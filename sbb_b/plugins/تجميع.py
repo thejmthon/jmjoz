cd854cfeba3eb19768fb99f892e274a24dfb1660
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-08 01:44:16 +0300

    Update تجميع.py

--- sbb_b/plugins/تجميع.py
+++ sbb_b/plugins/تجميع.py
@@ -1,6 +1,5 @@
 # t.me/Dar4k
 # this file for https://github.com/thejmthon/sbb_b0
-
 import asyncio
 import requests
 from telethon.tl.functions.messages import GetHistoryRequest
@@ -11,7 +10,7 @@ from sbb_b import sbb_b
 
 @sbb_b.ar_cmd(pattern="تجميع$")
 async def _(event):
-        await event.edit("**- يجب عليك الاشتراك بقنوات البوت لتفادي الاخطاء اولا**")
+        await event.edit("حسنا, تأكد من انك مشترك ب قنوات الاشتراك الاجباري لتجنب الأخطأء")
         channel_entity = await sbb_b.get_entity("@t06bot")
         await sbb_b.send_message('@t06bot', '/start')
         await asyncio.sleep(10)
@@ -20,13 +19,14 @@ async def _(event):
         await asyncio.sleep(10)
         msg1 = await sbb_b.get_messages('@t06bot', limit=1)
         await msg1[0].click(0)
+
         chs = 1
         for i in range(100):
             await asyncio.sleep(10)
             list = await sbb_b(GetHistoryRequest(peer=channel_entity, limit=1,offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
             if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await edit_or_reply(event, "**- لا توجد أي قنوات متاحة في البوت الان**")
+                await sbb_b.send_message(event.chat_id, f"مافي قنوات بلبوت")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
@@ -38,9 +38,8 @@ async def _(event):
                 msg2 = await sbb_b.get_messages('@t06bot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
-                await edit_or_reply(event, "**- تم بنجاح الاشتراك في {chs} من القنوات**")
+                await sbb_b.send_message(event.chat_id, f"تم الاشتراك في {chs} قناة")
             except:
-                await edit_or_reply(event, "**- لقد حدث خطأ ما يبدو أنه تم حظرك**")
+                await sbb_b.send_message(event.chat_id, f"خطأ , ممكن تبندت")
                 break
-        await edit_or_reply(event, "**- تم بنجاح الانتهاء من تجميع النقاط**")
-        
+        await sbb_b.send_message(event.chat_id, "تم الانتهاء من التجميع !")
