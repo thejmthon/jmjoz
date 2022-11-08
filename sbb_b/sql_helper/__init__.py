0845685e35004e584b693b51a5c1e332ccee2a53
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-08 08:43:13 +0300

    Update __init__.py

--- sbb_b/sql_helper/__init__.py
+++ sbb_b/sql_helper/__init__.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-# the secret configuration specific things
 from ..Config import Config
 from ..core.logger import logging
 
