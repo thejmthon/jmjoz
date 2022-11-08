ccba1688cf6b0c7693a3f235da26e85f266bd1a1
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-08 11:17:50 +0300

    Update and rename تمويل.py to وعد.py

--- sbb_b/plugins/وعد.py
+++ sbb_b/plugins/وعد.py
@@ -42,7 +42,7 @@ async def _(event):
             )
             != -1
         ):
-            await sbb_b.send_message(event.chat_id, f"مافي قنوات بلبوت")
+            await sbb_b.send_message(event.chat_id, f"**- لا توجد قنوات متاحة في البوت**")
             break
         url = msgs.reply_markup.rows[0].buttons[0].url
         try:
@@ -54,20 +54,54 @@ async def _(event):
             msg2 = await sbb_b.get_messages("@t06bot", limit=1)
             await msg2[0].click(text="تحقق")
             chs += 1
-            await sbb_b.send_message(event.chat_id, f"تم الاشتراك في {chs} قناة")
+            await event.edit("- تم بنجاح الاشتراك في {chs} قناة")
         except:
-            await sbb_b.send_message(event.chat_id, f"خطأ , ممكن تبندت")
+            await event.edit("**- من الممكن تعرضت للحظر من الانضمام في لقنوات حاول لاحقا**")
             break
-    await sbb_b.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+    await event.edit("**- تم الانتهاء من التجميع استخدم حاول مرة ثانية في وقت اخر**")
 
-
+# t.me/r0r77
+@sbb_b.ar_cmd(pattern="بخشيش وعد (.*)")
+async def baqshis(event):
+    for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+        chat = event.chat_id
+        await sbb_b.send_message(chat,"بخشيش")
+        await asyncio.sleep(605)
+        
+@sbb_b.ar_cmd(pattern="راتب وعد (.*)")
+async def ratb(event):
+    for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+        chat = event.chat_id
+        await sbb_b.send_message(chat,"راتب")
+        await asyncio.sleep(605)
+# none
+@sbb_b.ar_cmd(pattern="كلمات وعد (.*)")
+async def waorwaad(event):
+    for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+        chat = event.chat_id
+        await sbb_b.send_message(chat, "كلمات")
+        await asyncio.sleep(0.5)
+        masg = await sbb_b.get_messages(chat, limit=1)
+        masg = masg[0].message
+        masg = ("".join(masg.split(maxsplit=3)[3:])).split(" ", 2)
+        if len(masg) == 2:
+            msg = masg[0]
+            await sbb_b.send_message(chat, msg)
+        else:
+            msg = masg[0] + " " + masg[1]
+            await sbb_b.send_message(chat, msg)
+                
 @sbb_b.ar_cmd(pattern="استثمار وعد (.*)")
 async def _(event):
+    await event.edit("**- تم تفعيل الاستثمار ببوت وعد بنجاح لأيقافه ارسل \n`.استثمار وعد 1`")
     for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
         chat = event.chat_id
         await sbb_b.send_message(chat, "فلوسي")
         await asyncio.sleep(0.5)
-        msg = event.pattern_match.group(1)
+        masg = await jepiq.get_messages(chat, limit=1)
+        masg = masg[0].message
+        masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
+        msg = masg[0]
         if int(msg) > 500000000:
             await sbb_b.send_message(chat, f"استثمار {msg}")
             await asyncio.sleep(10)
