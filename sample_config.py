a014d8ed9b937c1c2b2926f51e422e4d6449e8c1
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-24 22:38:58 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -34,7 +34,7 @@ class Config(object):
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
     if bool(EXTERNAL_REPO and (EXTERNAL_REPO.lower() != "false")):
         if not url(EXTERNAL_REPO):
-            EXTERNAL_REPO = "https://github.com/sbb-b/JmPlugins"
+            EXTERNAL_REPO = "https://github.com/thejmthon/additional"
     else:
         EXTERNAL_REPO = None
     # فارات الميوزك
