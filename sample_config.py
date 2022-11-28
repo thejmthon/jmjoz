06d2545783bebcf0f52f1894985484d884050eed
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-11-28 17:13:29 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -37,7 +37,7 @@ class Config(object):
         EXTERNAL_REPO = None
     # فارات الميوزك
     VCMODE = os.environ.get("VCMODE", False)
-    PORT = os.environ.get("PORT", "8080")
+    #PORT = os.environ.get("PORT", "8080")
     VCMODE = bool(VCMODE and (VCMODE.lower() != "false"))
     VC_SESSION = os.environ.get("VC_SESSION", None)
     ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
