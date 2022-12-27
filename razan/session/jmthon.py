894849eb0242c09b1ef4d308e8c6ba04bf5fcbd7
jmthonr <thejmthon@users.noreply.github.com>
2022-12-27 07:11:59 +0000

    أعداد تلقائي

--- razan/session/jmthon.py
+++ razan/session/jmthon.py
@@ -1,5 +1,5 @@
+from os import system
 from time import sleep
-from os import system, path
 
 PANDA = "JMTHON USERBOT DEPLOY"
 
@@ -7,10 +7,19 @@ PANDA = "JMTHON USERBOT DEPLOY"
 def clear():
     system("clear")
 
+
 def jmthon():
     print(f"NOW PUT YOUR VARS CORRECTLY")
     with open(".env", "a") as file:
-        for var in ["ALIVE_AME", "APP_ID", "API_HASH", "STRING_SESSION", "DATABASE_URL", "TG_BOT_TOKEN", "TZ"]:
+        for var in [
+            "ALIVE_AME",
+            "APP_ID",
+            "API_HASH",
+            "STRING_SESSION",
+            "DATABASE_URL",
+            "TG_BOT_TOKEN",
+            "TZ",
+        ]:
             inpr = input(f"Enter {var}\n- ")
             file.write(f"{var}={inpr}\n")
     print("* Created '.env' file successfully *")
