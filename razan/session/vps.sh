dcd36b09c54cea731667a78a4de4de8d86da55bd
jmthonr <103285401+jmthonr@users.noreply.github.com>
2022-12-31 08:21:31 +0300

    i dont need py file to make env file
    also this maybe fix screen and run error

--- razan/session/vps.sh
+++ razan/session/vps.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 JMTHON="\nJMTHON USERBOT DEPLOY ON VPS"
 JMTHON+="\n "
 JMTHON+="\n "
@@ -9,34 +11,46 @@ echo -e $JMTHON
 echo -e $ROZ
 echo "WAIT ..."
 echo -e $ROZ
+
+# Update and install dependencies  :)
 sudo apt update && upgrade -y
-sudo apt install git -y
+sudo apt install --no-install-recommends -y \
+curl \
+git \
+libffi-dev \
+libjpeg-dev \
+libwebp-dev \
+python3-lxml \
+python3-psycopg2 \
+libpq-dev \
+libcurl4-openssl-dev \
+libxml2-dev \
+libxslt1-dev \
+python3-pip \
+python3-sqlalchemy \
+openssl \
+wget \
+python3 \
+python3-dev \
+libreadline-dev \
+libyaml-dev \
+gcc \
+zlib1g \
+ffmpeg \
+libssl-dev \
+libgconf-2-4 \
+libxi6 \
+unzip \
+libopus0 \
+libopus-dev \
+python3-venv \
+libmagickwand-dev \
+pv \
+tree \
+mediainfo \
+nano \
+nodejs
 clear
-echo -e $JMTHON
-echo -e $ROZ
-echo "installing python3"
-echo -e $ROZ
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install postgresql
-sudo apt install neofetch
-sudo apt install ffmpeg
-sudo apt install curl
-sudo apt install megatools
-sudo apt install unzip
-sudo apt install wget
-sudo apt install liblapack-dev
-sudo apt install aria2
-sudo apt install zip
-sudo apt install nano
-sudo apt install sudo
-sudo apt install python3-wand
-sudo apt install python3-lxml
-sudo apt install postgresql-client
-pip3 install av -q --no-binary av
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-echo -e $JMTHON
-echo -e $ROZ
 echo "⚙️ Github Installer"
 echo -e $ROZ
 echo -e $JMTHON
@@ -49,5 +63,42 @@ echo -e $ROZ
 echo "runing jmthon now"
 echo -e $ROZ
 cd sbb_b0
-pip3 install -r requirements.txt
-python3 jmthon.py
+
+# Rename sample_config.py to config.py
+mv sample_config.py config.py
+echo "⚙️ Environment "
+echo -e $ROZ
+
+# Ask the user for some environment variables and add them to .env
+if [ ! -f .env ]; then
+  touch .env
+fi
+
+echo "Enter your name:"
+read alive_name
+echo -e $ROZ
+echo "Enter app id:"
+read app_id
+echo -e $ROZ
+echo "Enter your api hash:"
+read api_hash
+echo -e $ROZ
+echo "Enter your session:"
+read session
+echo -e $ROZ
+echo "Enter your bot token:"
+read token
+echo -e $ROZ
+echo "Enter your database:"
+read database
+
+echo "ALIVE_NAME=$alive_name" >> .env
+echo "APP_ID=$app_id" >> .env
+echo "API_HASH=$api_hash" >> .env
+echo "STRING_SESSION=$session" >> .env
+echo "TG_BOT_TOKEN=$token" >> .env
+echo "DATABASE_URL=$database" >> .env
+clear
+
+# Install requirements and run the bot
+screen -S jmthon bash -c "virtualenv venv && source venv/bin/activate && pip3 install -r requirements.txt && clear && python3 -m sbb_b"
