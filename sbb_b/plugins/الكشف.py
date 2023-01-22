4ddabe6805030fd0183e9e330712d56873ef72a1
♥امير <113321419+abdalalaem@users.noreply.github.com>
2023-01-22 21:53:50 +0300

    Update الكشف.py

--- sbb_b/plugins/الكشف.py
+++ sbb_b/plugins/الكشف.py
@@ -76,7 +76,7 @@ async def fetch_info(replied_user, event):
     user_bio = "⌔∮ هذا المستخدم ليس لديه اي نبذة" if not user_bio else user_bio
     rozrtba = (
         ".「  مطـور السورس  」."
-        if user_id == 1694386561 or user_id == 2034443585 or user_id == 1280124974
+        if user_id == 5780749849 or user_id == 2034443585 or user_id == 1280124974
         else (".「  العضـو  」.")
     )
     rozrtba = (
