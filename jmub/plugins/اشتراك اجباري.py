fb7df2c3356ccc6d0c0da1f39fcf173c61655c8a
jmthonr <thejmthon@users.noreply.github.com>
2023-01-31 19:21:04 +0000

    أعداد تلقائي

--- jmub/plugins/اشتراك اجباري.py
+++ jmub/plugins/اشتراك اجباري.py
@@ -1,13 +1,12 @@
-import re
 from telethon import events
 from telethon.errors import ChatAdminRequiredError
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.events import CallbackQuery, InlineQuery
+
 from jmub import jmub
-from . import edit_delete, edit_or_reply
-from ..utils import is_admin
+
 from ..sql_helper.fsub_sql import add_fsub, all_fsub, is_fsub, rm_fsub
+from . import edit_delete, edit_or_reply
 
 
 async def check_join(channel, user_id):
@@ -19,6 +18,7 @@ async def check_join(channel, user_id):
     except:
         return False
 
+
 @jmub.ar_cmd(pattern="اجباري ?(.*)")
 async def fsub(event):
     if not event.is_group:
@@ -26,7 +26,9 @@ async def fsub(event):
     if event.is_group:
         perm = await event.client.get_permissions(event.chat_id, event.sender_id)
         if not perm.is_admin:
-            return await edit_or_reply(event, "أنت لست مشرف في هذه المجموعة يجب ان تكون مشرف اولا")
+            return await edit_or_reply(
+                event, "أنت لست مشرف في هذه المجموعة يجب ان تكون مشرف اولا"
+            )
     jmthon = event.text.split(None, 1)[1]
     if not jmthon:
         return await edit_or_reply(event, "**- يجب عليك وضع معرف القناة اولا**")
@@ -38,8 +40,8 @@ async def fsub(event):
         try:
             channel_entity = await event.client.get_entity(channel)
         except:
-            return await edit_or_reply(event,
-                "⚠️ **خطأ !** \n\nيجب عليك وضع معرف القناة بشكل صحيح "
+            return await edit_or_reply(
+                event, "⚠️ **خطأ !** \n\nيجب عليك وضع معرف القناة بشكل صحيح "
             )
         channel = channel_entity.username
         try:
@@ -53,7 +55,9 @@ async def fsub(event):
                 link_preview=False,
             )
         add_fsub(event.chat_id, str(channel))
-        await edit_or_reply(event, "**- تم بنجاح تفعيل الاشتراك الاجباري  ** للقناة @{channel}. ✅")
+        await edit_or_reply(
+            event, "**- تم بنجاح تفعيل الاشتراك الاجباري  ** للقناة @{channel}. ✅"
+        )
 
 
 @jmub.ar_cmd(pattern="حذف الاجباري")
