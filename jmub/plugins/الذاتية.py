6eec83bcf1861a3d983240b93df3f4d4303daa8c
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-01 16:45:29 +0300

    Update الذاتية.py

--- jmub/plugins/الذاتية.py
+++ jmub/plugins/الذاتية.py
@@ -1,6 +1,9 @@
 from os import remove
-
+from telethon import events 
+import asyncio
 from jmub import jmub
+
+jmthonself = False
 
 
 @jmub.ar_cmd(pattern="(سي|ذاتية)")
@@ -10,3 +13,23 @@ async def datea(event):
     downloadjmthon = await scertpic.download_media()
     await jmub.send_file("me", downloadjmthon)
     remove(downloadjmthon)
+
+
+@jmub.ar_cmd(pattern="تفعيل الذاتية")
+async def start_downloader():
+    global jmthonself
+    jmthonself = True
+    await event.edit("- تم بنجاح تفعيل حفظ الميديا الذاتية من الان")
+
+@jmub.ar_cmd(pattern="تعطيل الذاتية")
+async def stop_downloader():
+    global jmthonself
+    jmthonself = False
+    await event.edit("- تم بنجاح تعطيل حفظ الميديا الذاتية من الان")
+
+@jmub.on(events.NewMessage(func=lambda e: e.is_private and (e.photo or e.video) and e.media_unread))
+async def downloader(event):
+    global jmthonself
+    if jmthonself:
+        result = await event.download_media()
+        await jmub.send_file("me", result, caption="- تم بنجاح الحفظ بواسطة @jmthon")
