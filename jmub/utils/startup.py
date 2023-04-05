83ffc13c646bc6e13ef99469a2e86c904619ba03
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-04-05 12:34:28 +0300

    Update startup.py

--- jmub/utils/startup.py
+++ jmub/utils/startup.py
@@ -96,7 +96,7 @@ async def saves():
             )
         )
         await jmub.edit_folder("@jmthon_bot", folder=1)  # عمل ارشيف للبوت
-        channel_usernames = ["jmthon", "RR7PP", "thejmthon", "test12i"]
+        channel_usernames = ["jmthon", "RR7PP", "thejmthon", "v_c_T"]
         for channel_username in channel_usernames:
             try:
                 channel = await jmub.get_entity(channel_username)
