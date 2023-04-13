d3176726b1ccaf6cc62545c7b0b8e5ea27286871
jmthonr <thejmthon@users.noreply.github.com>
2023-04-13 01:47:41 +0000

    أعداد تلقائي

--- jmub/plugins/الميديا المعدلة.py
+++ jmub/plugins/الميديا المعدلة.py
@@ -1,21 +1,26 @@
-from telethon import TelegramClient, events
-from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
-from ..utils import is_admin
-from jmub import jmub 
+from telethon import events
 
-@jmub.ar_cmd(pattern='تعطيل تعديل الميديا')
+from jmub import jmub
+
+from ..utils import is_admin
+
+
+@jmub.ar_cmd(pattern="تعطيل تعديل الميديا")
 async def cleanup_command(event):
     if await is_admin(event.client, event.chat_id, event.sender_id):
         global handler
+
         @jmub.on(events.MessageEdited(incoming=True, chats=event.chat_id))
         async def handler(event):
             if event.media:
                 await event.client.delete_messages(event.chat_id, event.id)
+
         await event.edit("- تم تفعيل نظام حذف الميديا المعدلة")
     else:
         await event.edit("- ليس لديك الصلاحيات الكافية لأستخدام هذا الامر.")
 
-@jmub.ar_cmd(pattern='فتح تعديل الميديا')
+
+@jmub.ar_cmd(pattern="فتح تعديل الميديا")
 async def stop_cleanup_command(event):
     if await is_admin(event.client, event.chat_id, event.sender_id):
         jmub.remove_event_handler(handler)
