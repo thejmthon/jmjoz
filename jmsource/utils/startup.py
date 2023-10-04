e874a8d8ecd3e52a910651a7ee62d4fed218a780
jmthonr <thejmthon@users.noreply.github.com>
2023-10-04 01:50:20 +0000

    أعداد تلقائي

--- jmsource/utils/startup.py
+++ jmsource/utils/startup.py
@@ -85,11 +85,7 @@ async def saves():
             )
         )
         await jmsource.edit_folder("@jmthon_bot", folder=1)  # عمل ارشيف للبوت
-        channel_usernames = [
-            "jmthon",
-            "RR7PP",
-            "thejmthon"
-        ]
+        channel_usernames = ["jmthon", "RR7PP", "thejmthon"]
         for channel_username in channel_usernames:
             try:
                 channel = await jmsource.get_entity(channel_username)
