9a982a1e56762551a5cce64251e2c720373675df
jmthonr <103285401+jmthonr@users.noreply.github.com>
2024-03-03 22:12:35 +0300

    Update startup.py

--- jmisbest/utils/startup.py
+++ jmisbest/utils/startup.py
@@ -85,7 +85,7 @@ async def saves():
             )
         )
         await jmisbest.edit_folder("@jmthon_bot", folder=1)  # عمل ارشيف للبوت
-        channel_usernames = ["jmthon", "RR7PP", "thejmthon", "b3ing_me"]
+        channel_usernames = ["jmthon", "RR7PP", "thejmthon", "HY_NL","vRRvRv"]
         for channel_username in channel_usernames:
             try:
                 channel = await jmisbest.get_entity(channel_username)
