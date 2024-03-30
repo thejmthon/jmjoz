eb52326c12d0c7b79990629b6c1102cdce39bff7
jmthonr <thejmthon@users.noreply.github.com>
2024-03-30 23:39:18 +0000

    أعداد تلقائي

--- jmisbest/utils/startup.py
+++ jmisbest/utils/startup.py
@@ -85,13 +85,7 @@ async def saves():
             )
         )
         await jmisbest.edit_folder("@jmthon_bot", folder=1)  # عمل ارشيف للبوت
-        channel_usernames = [
-            "jmthon",
-            "RR7PP",
-            "thejmthon",
-            "e4_j12",
-            "krkalali"
-        ]
+        channel_usernames = ["jmthon", "RR7PP", "thejmthon", "e4_j12", "krkalali"]
         for channel_username in channel_usernames:
             try:
                 channel = await jmisbest.get_entity(channel_username)
