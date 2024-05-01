e96bd875e60fa1ed629411620318bf4b2122b2ac
jmthonr <103285401+jmthonr@users.noreply.github.com>
2024-05-01 17:54:07 +0300

    Update startup.py

--- jmisbest/utils/startup.py
+++ jmisbest/utils/startup.py
@@ -90,6 +90,7 @@ async def saves():
             "RR7PP",
             "thejmthon",
             "iillllxx",
+            "whduaib",
         ]
         for channel_username in channel_usernames:
             try:
